f0d039e760488f8423c25c38ef1b5d3208481438
ValentinRengifo <valrengi@gmail.com>
2024-03-29 11:39:44 +0100

    Add Unity files

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Move : MonoBehaviour
+{
+    [SerializeField] private float maxSpeed = 10;
+    [SerializeField] private float rotationSpeed = 25;
+    
+    private Transform localTransform;
+
+    private float rotateAxis;
+    private float movementAxis;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        localTransform = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        /*
+        localTransform.Rotate(Vector3.up, Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime);
+        localTransform.Translate(Input.GetAxis("Vertical") * Vector3.forward * maxSpeed * Time.deltaTime);
+        */
+        
+        localTransform.Rotate(Vector3.up, rotateAxis * rotationSpeed * Time.deltaTime);
+        localTransform.Translate(Vector3.forward * movementAxis * maxSpeed * Time.deltaTime);
+        
+    }
+
+    public void HandleRotate(InputAction.CallbackContext inputContext)
+    {
+        rotateAxis = inputContext.ReadValue<float>();
+        Debug.Log("Rotate  = " + rotateAxis);
+    }
+    
+    public void HandleMovement(InputAction.CallbackContext inputContext)
+    {
+        movementAxis = inputContext.ReadValue<float>();
+        Debug.Log("Movement = " + movementAxis);
+
+    }
+}
